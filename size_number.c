40e0957931709a95e635e0db88f56c51cbab0a88
Salma <salmaelaadam@gmail.com>
2023-04-25 12:27:28 +0100

    done

---
+++ size_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * con_size_num - Casts a number to the specified size
+ * @num: Number to be casted.
+ * @size: Number indicating the type to be casted.
+ *
+ * Return: Casted value of num
+ */
+long int con_size_num(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
---
+++ size_unsigned.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * con_size_unsign - Casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
+ *
+ * Return: Casted value of num
+ */
+long int con_size_unsign(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
